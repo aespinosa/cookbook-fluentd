ca7065dc963962a2c7dfe22af4851b291d491954
Allan Espinosa <allan.espinosa@outlook.com>
2017-05-03 23:07:46 -0500

    Initialize the cookbook

---
+++ metadata.rb
@@ -0,0 +1,16 @@
+name 'fluentd'
+version '0.0.1'
+
+maintainer 'Allan Espinosa'
+maintainer_email 'allan.espinosa@outlook.com'
+
+issues_url 'https://github.com/aespinosa/cookbook-fluentd/issues'
+source_url 'https://github.com/aespinosa/cookbook-fluentd'
+
+chef_version '>= 12.5'
+
+license 'Apache-2.0'
+
+supports 'debian', '>= 8.0'
+supports 'centos', '>= 7.0'
+supports 'ubuntu', '>= 16.04'
